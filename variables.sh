7c591e261969f70794a09b3849c9023649aac108
Devendar30 <cheruku.devendar@gmail.com>
2021-03-06 16:49:46 +0530

    New sample.sh

--- variables.sh
+++ variables.sh
@@ -16,9 +16,10 @@ NAME="welcome to devops"
 echo Name=$NAME
 
 DATE=$(date)
+DAY_TIME=$(date +%date)
 
 echo "today date is ${DATE}".
-echo "today date is ${DATE +%F}."
+echo "today date is ${DAY_TIME}"
 
 ADD=$((2+3+4+5))
 CALC=$((1+5-3+7/3))
