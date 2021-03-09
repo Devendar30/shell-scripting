4e52b63fe002aa11d69c81829a055b4f0b7f0cda
Devendar30 <cheruku.devendar@gmail.com>
2021-03-09 16:21:20 +0530

    -ne

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -3,7 +3,7 @@
 USER_ID=$(id -u)
 
 if [ "USER_ID" != 0 ]; then
-  echo -e "\e[31You must be root user to execute this script\e[0m"
+  echo -e "\e[31mYou must be root user to execute this script\e[0m"
 fi
 
 
