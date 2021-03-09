2bbcbea5c183e4c65d94e42fb0019b78b348a75d
Devendar30 <cheruku.devendar@gmail.com>
2021-03-09 17:13:19 +0530

    -ne

--- roboshop/components/common.sh
+++ roboshop/components/common.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-echo i am common
+USER_ID=$(id -u)
 
-#USER_ID=$(id -u)
-
-#if [ "USER_ID" != 0 ]; then
-#  echo -e "\e[1;31mYou must be root user to execute this script\e[0m"
-#  exit 1
-#fi
+if [ "USER_ID" != 0 ]; then
+  echo -e "\e[1;31mYou must be root user to execute this script\e[0m"
+  exit 1
+fi
 
 #Print() {
 #  echo -e "\e[1m $(date +%c) \e[35m$(hostname)\e[0m \e[1;36m${COMPONENT} :: $1"
