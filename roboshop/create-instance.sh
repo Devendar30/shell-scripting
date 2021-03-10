e6b1f00094d310f9e56ffb81544b22d92e7f504f
Devendar30 <cheruku.devendar@gmail.com>
2021-03-10 13:36:04 +0530

    New sample.sh

--- roboshop/create-instance.sh
+++ roboshop/create-instance.sh
@@ -9,7 +9,9 @@ fi
 
 STATE=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=${component}" --query 'Reservations[*].Instances[*].State.Name' --output text)
 
-  if [ $STATE != "running" ]; then
+#  if [ $STATE != "running" ]; then
+
+if [ $STATE != "running" ]; then
 aws ec2 run-instances --launch-template LaunchTemplateId=lt-061f7c4e2482ab086 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${component}}]"
 fi
 
