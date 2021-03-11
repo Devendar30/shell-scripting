1b690f32902a92c6cdd053e2626e209a53c6ded5
Devendar30 <cheruku.devendar@gmail.com>
2021-03-11 10:25:11 +0530

    New sample.sh

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 
-echo Welcome to Catalogue.
+COMPONENT=catalogue
+
+source components/common.sh
+
+Print "Install nodejs" "Yum Install nodejs"
+
+yum install nodejs make gcc-c++ -y
+stat $?
+
+Print "Adding Roboshop user"
+useradd roboshop
+stat $?
+
+Print "Download catalogue component code"
+
+$ curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip"
+Print "extarct catalogue component code" "rm -rf /home/roboshop/catalogue && cd /home/roboshop/catalogue && unzip /tmp/catalogue.zip"
+
+rm -rf /home/roboshop/catalogue && mkdir -p /home/roboshop/catalogue && cd /home/roboshop/catalogue && unzip /tmp/catalogue.zip
+stat $?
+
+exit
+$ npm install
+
+ #mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
+ #systemctl daemon-reload
+ #systemctl start catalogue
+#systemctl enable catalogue
